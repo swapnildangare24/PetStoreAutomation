7165e2c48548d2b4163a1c79c4f38e1ef86b66e8
swapnil <swapnildangare24@outlook.com>
2023-05-17 12:08:02 +0530

    this is my first commit

---
+++ src/test/java/api/endpoints/Routes.java
@@ -0,0 +1,27 @@
+package api.endpoints;
+
+//swagger url ="https://petstore.swagger.io"
+
+public class Routes {
+	
+	public static String base_url = "https://petstore.swagger.io/#";
+
+	//user module
+	
+	public static String post_url= base_url+"/user";
+	
+	public static String get_url= base_url+"/user/{username}";
+	
+	public static String update_url= base_url+"/user/{username}";
+	
+	public static String delete_url= base_url+"/user/{username}";
+	
+	//store modules
+	
+	//Here we create Store module URL's
+	
+	//pet modules
+	
+		//Here we create pet Store module URL's
+	
+}
